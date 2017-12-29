6878b7664e4f36bf4b67a91ab27080fe4c33b439
Mateusz Matocha <mateuszmatocha@gmail.com>
2017-12-29 20:36:21 +0100

    Base project set up

---
+++ src/main/java/pl/edu/pb/wi/project/database/DatabaseApplication.java
@@ -0,0 +1,12 @@
+package pl.edu.pb.wi.project.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseApplication.class, args);
+	}
+}
