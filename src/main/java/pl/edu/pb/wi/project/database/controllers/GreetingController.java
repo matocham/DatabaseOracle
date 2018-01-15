6961f9efd0afa8611689b42c1fe63bba8435112a
Mateusz Matocha <mateuszmatocha@gmail.com>
2018-01-15 18:16:50 +0100

    Added some resources

--- src/main/java/pl/edu/pb/wi/project/database/controllers/GreetingController.java
+++ src/main/java/pl/edu/pb/wi/project/database/controllers/GreetingController.java
@@ -11,22 +11,16 @@ import javax.sql.DataSource;
 
 @Controller
 public class GreetingController {
-
-    @Autowired
-    DataSource dataSource;
-
     @Autowired
     UsersRepository usersRepository;
 
-    /*
-    @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        System.out.println("DATASOURCE = " + dataSource);
-        model.addAttribute("allUsers", usersRepository.findAll());
-        model.addAttribute("foundUser", usersRepository.findByEmail("bbrzozowski@examle.com"));
-        usersRepository.finalizeExchange(2L);
-        return "greeting";
+    @RequestMapping("/login")
+    public String greeting() {
+        return "login";
     }
-    */
+
+    @RequestMapping("/category")
+    public String greeting2() {
+        return "category";
+    }
 }
